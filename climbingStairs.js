7bfdcabcb5d96eac4b4f212c2abc5d38ada259e0
changchang <pengchang1997@126.com>
2021-08-05 15:51:51 +0800

    爬楼梯

---
+++ climbingStairs.js
@@ -0,0 +1,25 @@
+// Leetcode 70
+
+// 爬楼梯
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n === 1) {
+        return 1;
+    }
+
+    if (n === 2) {
+        return 2;
+    }
+
+    const dp = new Array(n + 1);
+    dp[1] = 1; dp[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+};
