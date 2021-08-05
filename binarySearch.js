6881f639543c13edb40e5fae4b4594c54355f3a1
changchang <pengchang1997@126.com>
2021-08-06 06:44:25 +0800

    二分查找

---
+++ binarySearch.js
@@ -0,0 +1,24 @@
+// Leetcode 704
+
+// 二分查找
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let low = 0, high = nums.length - 1;
+    while (low <= high) {
+        let mid = Math.floor(low + high, 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return -1;
+};
