4dd398e8f4bb61e8a35926093d0b8e32732d6f5a
changchang <pengchang1997@126.com>
2021-08-07 13:33:47 +0800

    斐波那契数

---
+++ fibonacciNumber.js
@@ -0,0 +1,25 @@
+// Leetcode 509
+
+// 斐波那契数
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if (n === 0) {
+        return 0;
+    }
+
+    if (n === 1) {
+        return 1;
+    }
+
+    const dp = new Array(n + 1);
+    dp[0] = 0; dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+};
